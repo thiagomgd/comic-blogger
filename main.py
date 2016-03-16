a2624e6b93036db4a28967052eb403566ad956c6
Thiago Margarida <thiago.margarida@controller.education>
2016-03-16 16:38:23 -0300

    .

--- main.py
+++ main.py
@@ -9,15 +9,30 @@ import requests
 #http://josephephillips.com/comic_vine_api_examples_part2/
 #http://www.comicvine.com/forums/api-developers-2334/the-new-api-same-as-the-old-api-except-for-the-dif-1449264/
 
+headers = {"User-Agent": "Comic Listing by Thiago"}
+
+def check_tpb_hc(i):
+    return ("name" in i and i["name"] != None and (
+                "Vol" in i["name"] or
+                "HC" in i["name"] or
+                "TPB" in i["name"]))
+
+def check_first_issue(i):
+    #return (not("name" in i and i["name"] != None and (
+    #            "Vol" in i["name"] or
+    #            "HC" in i["name"] or
+    #            "TPB" in i["name"])) and i["issue_number"] == "1")
+    return i["issue_number"] == "1"
+
 def get_issues():
     url = issues_url.format(apikey, date, sort, res_format, field_list, offset.format(off_set))
-    r = requests.get(url, timeout=30)
+    r = requests.get(url, timeout=30, headers=headers)
     content = json.loads(r.content.decode("utf-8"))
     return content["results"]
 
 def get_volume(api_detail_url):
     url = "{}?api_key={}{}".format(api_detail_url, apikey, res_format)
-    r = requests.get(url, timeout=30)
+    r = requests.get(url, timeout=30, headers=headers)
     content = json.loads(r.content.decode("utf-8"))
     return content["results"]
 
@@ -30,7 +45,7 @@ issues = {}
 
 item_layout = config["item_layout"]
 apikey = config["apikey"]
-date = "&filter=store_date:2016-02-03"
+date = "&filter=store_date:2016-02-21|2016-02-27"
 sort = "&sort=name"
 offset = "&offset={}"
 off_set = 0
@@ -44,10 +59,8 @@ results = get_issues()
 
 while results:
     for i in results:
-        if ("name" in i and i["name"] != None and (
-                "Vol" in i["name"] or
-                "HC" in i["name"] or
-                "TPB" in i["name"])):
+        #if check_first_issue(i):
+        if check_tpb_hc(i):
 
             volume = get_volume(i["volume"]["api_detail_url"])
 
@@ -66,14 +79,22 @@ while results:
 order = [
 'Marvel',
 'DC Comics',
+'Vertigo',
 'Image',
 'IDW Publishing',
 'Dynamite Entertainment',
 'Dark Horse Comics',
 'Boom! Studios',
 'Archie Comics',
+'Titan Comics',
+'Valiant',
+'Seven Seas Entertainment',
+'Yen Press',
+'Kodansha',
 'Kodansha Comics USA',
+'Kadokawa Shoten',
 'Action Lab',
+'UDON',
 'Dark Horse Manga',
 'Delcourt',
 'Antarctic Press',
@@ -82,13 +103,24 @@ order = [
 'Skybound',
 'Viz',
 'First Second Books',
-'Les Humanoïdes Associés'
+'Akita Shoten',
+'Tokuma Shoten',
+'Vertical Inc.',
+'Netcomics',
+'American Gothic Press',
+'Les Humanoïdes Associés',
+'Andrews McMeel Publishing'
 ]
 
 for pub in order:
     if pub in issues:
-        pub_text = "<h4>Editora: {}</h4>\n\n{}".format(pub, "\n".join(issues[pub]))
+        pub_text = "<h4>Editora: {}</h4>\n{}<br/><br/>".format(pub, "\n<br/>".join(issues[pub]))
         arcticle = "{}{}\n\n".format(arcticle, pub_text)
+        issues[pub] = None
+
+for i in issues:
+    if issues[i] != None:
+        print(i)
 
 with open('arcticle.txt', 'wb') as outfile:
     outfile.write(bytes(arcticle, 'UTF-8'))
